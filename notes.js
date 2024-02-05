3aa8c24b260ff7678ef6f5a8cdb8b9cc68103e75
Ritesh <riteshkd2k20@gmail.com>
2024-02-05 20:27:18 +0530

    delete fixed

--- notes.js
+++ notes.js
@@ -1,9 +1,22 @@
+function update() {
+  let text = document.querySelector(".text");
+  localStorage.setItem("notes", text);
+  console.log(localStorage.getItem("notes"));
+}
+
+function shownotes() {
+  let text = document.querySelector(".text");
+  text.innerHTML = localStorage.getItem("1");
+  console.log("kuch ho bhi rha h");
+}
+
 function addpage() {
   let page = document.createElement("div");
   let p = document.createElement("p");
   let img = document.createElement("img");
   img.src = "skull.png";
   img.className = "remove";
+  // img.onclick = del;
   p.contentEditable = "true";
   page.className = "page";
   p.className = "text";
@@ -11,12 +24,29 @@ function addpage() {
   document.body.appendChild(page);
   page.appendChild(p);
   page.appendChild(img);
-  console.log("succcess");
+  console.log("success");
 }
-let skulldel = document.querySelector(".remove");
-skulldel.addEventListener("click", del);
 
-function del() {
-  alert("hii");
-  console.log("hiiii");
-}
+// function del() {
+//   let img = document.querySelector(".remove");
+//   let page = img.closest(".page");
+//   page.remove();
+//   update();
+// }
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.className === "remove") {
+    e.target.parentElement.remove();
+    // alert("if works");
+    update();
+  } else if (e.target.className === "text") {
+    console.log("if works");
+    let notes = document.querySelectorAll(".text");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        update();
+        console.log("update while writing");
+      };
+    });
+  }
+});
